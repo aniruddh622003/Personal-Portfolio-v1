b0a875588d45a045ae85502e2aac22c13bd6cbb1
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2022-06-18 03:41:18 +0530

    initial setup done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { CacheProvider } from "@emotion/react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import createEmotionCache from "../utils/createEmotionCache";
+import { theme } from "../utils/theme";
+import Header from "../components/shared/Header";
+import Footer from "../components/shared/Footer";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ utils/theme.js
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#53dd30",
+    },
+    secondary: {
+      main: "#000000",
+    },
+    background: {
+      default: "#000000",
+      paper: "#2f2f2f",
+    },
+    text: {
+      primary: "#ffffff",
+    },
+  },
+});
